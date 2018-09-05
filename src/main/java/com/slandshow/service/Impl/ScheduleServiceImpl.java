c2e2e7c2e4452ea876d7fbdf2385ccf2b8a11306
SlandShow <slandshow12345@mail.ru>
2018-09-05 20:30:57 +0300

    Fix service in DTO case

--- src/main/java/com/slandshow/service/Impl/ScheduleServiceImpl.java
+++ src/main/java/com/slandshow/service/Impl/ScheduleServiceImpl.java
@@ -1,12 +1,14 @@
 package com.slandshow.service.Impl;
 
 import com.slandshow.DAO.ScheduleDAO;
+import com.slandshow.DTO.ScheduleDTO;
 import com.slandshow.models.Schedule;
 import com.slandshow.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.text.ParseException;
 import java.util.List;
 
 @Service
@@ -23,5 +25,21 @@ public class ScheduleServiceImpl implements ScheduleService {
     @Transactional
     public List<Schedule> getAllSchedules() {
         return scheduleDAO.getAll();
+    }
+
+    public List<ScheduleDTO> getDirectSchedulesFromDTOByStationsAndDates(ScheduleDTO scheduleDTO) throws ParseException {
+        return null;
+    }
+
+    public List<ScheduleDTO> getDirectSchedulesFromDTOByStations(ScheduleDTO scheduleDTO) throws ParseException {
+        return null;
+    }
+
+    public List<ScheduleDTO> getDirectSchedulesFromDTOByTrain(ScheduleDTO scheduleDTO) throws ParseException {
+        return null;
+    }
+
+    public List<ScheduleDTO> getDirectSchedulesFromDTOByDates(ScheduleDTO scheduleDTO) throws ParseException {
+        return null;
     }
 }
