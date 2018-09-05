18c647b0c5cbd7b54c06600d4abce543c42dcd4b
SlandShow <slandshow12345@mail.ru>
2018-09-05 14:54:43 +0300

    New JSP's

--- src/main/java/com/slandshow/utils/JspFormNames.java
+++ src/main/java/com/slandshow/utils/JspFormNames.java
@@ -7,4 +7,6 @@ public class JspFormNames {
     public static final String REGISTRATION_FORM = "helloworld-form";
     public static final String REGISTRATION_RESULT_FORM = "helloworld";
     public static final String STATIONS_VIEW_LIST = "stations-list";
+    public static final String SCHEDULE_INPUT_FOR_STATIONS_AND_DATE = "actual-train-traffic";
+    public static final String SCHEDULE_INPUT_FOR_STATIONS_AND_DATE_RESULT = "schedule-list";
 }
