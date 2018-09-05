c5712ef09d346743fa6cf875d6de8f4e37e23ed8
SlandShow <slandshow12345@mail.ru>
2018-09-05 20:32:00 +0300

    Fix service interface in DTO case

--- src/main/java/com/slandshow/service/ScheduleService.java
+++ src/main/java/com/slandshow/service/ScheduleService.java
@@ -1,8 +1,10 @@
 package com.slandshow.service;
 
+import com.slandshow.DTO.ScheduleDTO;
 import com.slandshow.models.Schedule;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.List;
 
 @Service
@@ -11,4 +13,13 @@ public interface ScheduleService {
     List<Schedule> getByStationsAndDate(Schedule schedule);
 
     List<Schedule> getAllSchedules();
+
+    List<ScheduleDTO> getDirectSchedulesFromDTOByStationsAndDates(ScheduleDTO scheduleDTO) throws ParseException;
+
+    List<ScheduleDTO> getDirectSchedulesFromDTOByStations(ScheduleDTO scheduleDTO) throws ParseException;
+
+    List<ScheduleDTO> getDirectSchedulesFromDTOByTrain(ScheduleDTO scheduleDTO) throws ParseException;
+
+    List<ScheduleDTO> getDirectSchedulesFromDTOByDates(ScheduleDTO scheduleDTO) throws ParseException;
+
 }
