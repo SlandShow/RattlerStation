39cc024d545f48ecb366f26e9af282f162d3effd
SlandShow <slandshow12345@mail.ru>
2018-09-07 01:12:59 +0300

    Add second request mapping param to Home Page controller

--- src/main/java/com/slandshow/controllers/HomeController.java
+++ src/main/java/com/slandshow/controllers/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-    // Handle root request
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    // Handle root & home request
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String showHomePage() {
         return JspFormNames.MAIN_PAGE;
     }
