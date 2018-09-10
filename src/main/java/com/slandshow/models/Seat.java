c64aa7db5e751339eeeb0528d5b5ad43717faad3
SlandShow <slandshow12345@mail.ru>
2018-09-10 23:44:48 +0300

    Add new Entities: Seat & Ticket

---
+++ src/main/java/com/slandshow/models/Seat.java
@@ -0,0 +1,74 @@
+package com.slandshow.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "seat")
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "carriage")
+    private Integer carriage;
+
+    @Column(name = "seat")
+    private Integer seat;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "train_id", nullable = false)
+    private Train train;
+
+    public Seat() {
+    }
+
+    public Seat(Integer carriage, Integer seat, Train train) {
+        this.carriage = carriage;
+        this.seat = seat;
+        this.train = train;
+    }
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCarriage(Integer carriage) {
+        this.carriage = carriage;
+    }
+
+    public void setSeat(Integer seat) {
+        this.seat = seat;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    // Getters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getCarriage() {
+        return carriage;
+    }
+
+    public Integer getSeat() {
+        return seat;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    @Override
+    public String toString() {
+        return "Train ( id: " + id + " carriage: " + carriage + " seats: " + seat + " train: < " + train + " >)";
+    }
+}
---
+++ src/main/java/com/slandshow/models/Ticket.java
@@ -0,0 +1,94 @@
+package com.slandshow.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ticket")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "schedule_id", referencedColumnName = "id")
+    private Schedule schedule;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "seat_id", referencedColumnName = "id")
+    private Seat seat;
+
+    @Column(name = "price")
+    private Integer price;
+
+    public Ticket() {
+    }
+
+    public Ticket(Schedule schedule, User user, Seat seat, Integer price) {
+        this.schedule = schedule;
+        this.user = user;
+        this.seat = seat;
+        this.price = price;
+    }
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    // Getters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket ( id: "
+                + id
+                + " schedule: < "
+                + schedule + " > user: < "
+                + user + " > seat : < "
+                + seat + " > price: " + price
+                + " )";
+    }
+}
