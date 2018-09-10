fbf116720f78ddb50c04b65bcb6814a0d21be9f3
SlandShow <slandshow12345@mail.ru>
2018-09-10 23:48:00 +0300

    Add more functionality to ScheduleService

--- src/main/java/com/slandshow/service/ScheduleService.java
+++ src/main/java/com/slandshow/service/ScheduleService.java
@@ -2,19 +2,46 @@ package com.slandshow.service;
 
 import com.slandshow.DTO.ScheduleDTO;
 import com.slandshow.models.Schedule;
+import com.slandshow.models.Station;
 import org.springframework.stereotype.Service;
-
+import java.io.IOException;
 import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public interface ScheduleService {
 
-    List<Schedule> getByStationsAndDate(Schedule schedule);
+    void add(ScheduleDTO scheduleDTO) throws ParseException, IOException, TimeoutException;
+
+    void delete(Long id) throws IOException, TimeoutException;
+
+    void update(ScheduleDTO scheduleDTO) throws ParseException, IOException, TimeoutException;
 
     List<Schedule> getAllSchedules();
 
-    List<ScheduleDTO> getDirectSchedulesFromDTOByStationsAndDates(ScheduleDTO scheduleDTO) throws ParseException;
+    List<ScheduleDTO> getAll();
+
+    List<ScheduleDTO> getAllForToday() throws ParseException;
+
+    Schedule getById(Long id);
+
+    List<Schedule> getByDate(Date dateDeparture);
+
+    List<Schedule> getByDates(Date dateDeparture, Date dateArrival) throws ParseException;
+
+    List<Schedule> getByStationsAndDate(Schedule schedule);
+
+    List<Schedule> getByDateAndTrainToCheckIntersection(Schedule schedule);
+
+    List<Schedule> getByTrainAndDate(Schedule schedule);
+
+    List<Schedule> getByStationArrivalAndDate(Schedule schedule);
+
+    //List<TransferScheduleDTO> getTransferSchedules(ScheduleDTO scheduleDTO) throws ParseException, BusinessLogicException;
+
+    List<ScheduleDTO> getDirectSchedulesFromDTOByStationsAndDatesAndTrain(ScheduleDTO scheduleDTO) throws ParseException;
 
     List<ScheduleDTO> getDirectSchedulesFromDTOByStations(ScheduleDTO scheduleDTO) throws ParseException;
 
@@ -22,4 +49,11 @@ public interface ScheduleService {
 
     List<ScheduleDTO> getDirectSchedulesFromDTOByDates(ScheduleDTO scheduleDTO) throws ParseException;
 
+    //SeatsDTO getSeats(Long id);
+
+    List<Schedule> getByStationArrivalAndDates(Station station, Date dateFrom, Date dateTo);
+
+    //List<ScheduleMapDTO> getByRealTimeSchedules() throws ParseException;
+
+    ScheduleDTO getByIdScheduleDTO(Long id);
 }
