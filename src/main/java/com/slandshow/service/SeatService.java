1e78f15845253e3ef490f4440b238e4201811be0
SlandShow <slandshow12345@mail.ru>
2018-09-10 23:48:42 +0300

    Add buisness logic: Seat & Ticket Services

---
+++ src/main/java/com/slandshow/service/SeatService.java
@@ -0,0 +1,22 @@
+package com.slandshow.service;
+
+import com.slandshow.models.Seat;
+import com.slandshow.models.Train;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public interface SeatService {
+
+    void add(Seat seat);
+
+    void delete(Seat seat);
+
+    void update(Seat seat);
+
+    List<Seat> getAll();
+
+    Seat getById(Long id);
+
+    Seat getByTrainAndCarriageAndSeat(Train train, Integer carriage, Integer seat);
+}
---
+++ src/main/java/com/slandshow/service/TicketService.java
@@ -0,0 +1,48 @@
+package com.slandshow.service;
+
+import com.slandshow.DTO.TicketDTO;
+import com.slandshow.DTO.TicketInfoDTO;
+import com.slandshow.models.Schedule;
+import com.slandshow.models.Seat;
+import com.slandshow.models.Ticket;
+import com.slandshow.models.User;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public interface TicketService {
+
+    void add(Ticket ticket);
+
+    Ticket add(TicketDTO ticketDTO, User user) throws IOException, ParseException;
+
+    void delete(Ticket ticket);
+
+    void update(Ticket ticket);
+
+    List<Ticket> getAll();
+
+    Ticket getById(Long id);
+
+    List<Seat> getBookingSeatsBySchedule(Schedule schedule);
+
+    boolean checkUserUntilBooking(User user, Schedule schedule);
+
+    boolean checkSeatUntilBooking(Seat seat, Schedule schedule);
+
+    boolean checkScheduleForAvailability(Schedule schedule) throws ParseException;
+
+    List<Ticket> getBySchedules(Schedule schedule);
+
+    List<TicketInfoDTO> getByScheduleId(Long id);
+
+    List<TicketInfoDTO> getAuthenticatedUserTicket();
+
+    List<Ticket> getByDate(Date date);
+
+    List<Ticket> getByDates(Date dateFrom, Date dateTo);
+}
