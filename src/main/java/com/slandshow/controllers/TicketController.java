06fb055e605d8ff04ee84df969d8b38a724f56ca
SlandShow <slandshow12345@mail.ru>
2018-10-18 13:02:08 +0300

    Update ticket controller: remove legacy code from controller level, adapt controller for puzzled schedulers

--- src/main/java/com/slandshow/controllers/TicketController.java
+++ src/main/java/com/slandshow/controllers/TicketController.java
@@ -16,12 +16,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("tickets")
@@ -49,7 +52,7 @@ public class TicketController {
 
 
     @PostMapping("/buyTicket")
-    public String scheduleByStationsAndDatePersist(@ModelAttribute ScheduleDTO schedule, BindingResult result, Model model) {
+    public String scheduleByStationsAndDatePersist(@ModelAttribute ScheduleDTO schedule, HttpSession session, Model model) {
         Schedule reloadedSchedule = null;
 
         LOGGER.info(
@@ -61,95 +64,64 @@ public class TicketController {
 
         graphService.buildGraph();
 
+        List<ScheduleDTO> schedulers = null;
 
         try {
-            LOGGER.info("SEARCHED INFO VIA GRAPH: "
-                    + graphService.puzzleSchedules(
-                        graphService.parsePath(
-                                graphService.searchEdges(schedule.getStationDepartureName().intern(), schedule.getStationArrivalName().intern())
-                        ),
-                        schedule.getDateDeparture().intern(),
-                        schedule.getDateArrival().intern()
-                    )
+
+            List<Schedule> list =  graphService.puzzleSchedules(
+                    graphService.parsePath(
+                            graphService.searchEdges(schedule.getStationDepartureName().intern(), schedule.getStationArrivalName().intern())
+                    ),
+                    schedule.getDateDeparture().intern(),
+                    schedule.getDateArrival().intern()
             );
+
+            LOGGER.info("SEARCHED INFO VIA GRAPH: " + list);
+
+
+            Map<ScheduleDTO, List<Schedule>> map = graphService.filter(list);
+            for (Map.Entry<ScheduleDTO, List<Schedule>> entry : map.entrySet()) {
+                LOGGER.info("SELECTED PUZZLED SCHEDULER:" + entry.getKey().getTrainName() + ":" + entry.getValue().toString());
+            }
+
+            // Add map to session
+            session.setAttribute("map", map);
+
+            schedulers = graphService.parsedListFromMap(map);
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
+        if (schedulers != null)
+            model.addAttribute("schedules", schedulers);
 
-        try {
-            reloadedSchedule = scheduleService.mapping(schedule);
-
-            LOGGER.info("PARSED: Dates is "
-                    + reloadedSchedule.getDateDeparture() + ", "
-                    + reloadedSchedule.getDateArrival() + "\n Stations is "
-                    + reloadedSchedule.getStationDeparture() + ", "
-                    + reloadedSchedule.getStationArrival()
-            );
-
-        } catch (ParseException e) {
-            LOGGER.debug("Problem with parsing");
-        }
-
-        List<Schedule> schedules = null;
-
-        if (reloadedSchedule.getDateArrival() == null)
-            schedules = scheduleService.getByStationsAndDate(reloadedSchedule);
-        else schedules = scheduleService.getByStationsAndDates(reloadedSchedule);
-
-        model.addAttribute("schedules", schedules);
 
         return JspFormNames.BOOKING_TICKET_LIST;
     }
 
+    @RequestMapping(value = "/viewTicketsTrainInfo", params = {"train", "start", "end"})
+    public String confirmBooking(Model model, HttpSession session, @RequestParam(value = "train") String train, @RequestParam(value = "start") String start,  @RequestParam(value = "end") String end) {
 
-    /*
-    @Autowired
-    private TrainService trainService;
-    @GetMapping("/test")
-    public String test(Model model) {
-        Train selected = trainService.getByName("Train#138");
-        List<List<SeatDTO>> carriages = new ArrayList<List<SeatDTO>>();
+        // Get map with all puzzled schedulers from session
+        Map<ScheduleDTO, List<Schedule>> map = (Map<ScheduleDTO, List<Schedule>>) session.getAttribute("map");
 
-        int seatsCount = 1;
+        // Select value from map - list of picked schedulers (using params in URL)
+        List<Schedule> puzzledSchedulers = map.get(
+                new ScheduleDTO(train)
+        );
 
-        for (int carriagesIterator = 1; carriagesIterator <= selected.getCarriages(); carriagesIterator++) {
-            ArrayList<SeatDTO> seats = new ArrayList<SeatDTO>();
-            for (int seatsIterator = 1; seatsIterator <= (selected.getSeats().size() / selected.getCarriages()); seatsIterator++) {
-                seats.add(new SeatDTO(carriagesIterator, seatsCount++));
-            }
-
-            carriages.add(seats);
-        }
-
-        model.addAttribute("carriages", carriages);
-        Schedule selectedSchedule = scheduleService.getById(1l);
-        model.addAttribute("selectedSchedule", selectedSchedule);
-
-        return "train-seats-booking-info";
-    }
-*/
-
-
-
-    @RequestMapping(value = "/viewTicketsTrainInfo", params = "id")
-    public String confirmBooking(@RequestParam(value = "id") Long id, Model model) {
-        Schedule selectedSchedule = scheduleService.getById(id);
-        //TicketInfoDTO ticketInfoDTO = (TicketInfoDTO) request.getSession().getAttribute("ticketDTO");
-        List<List<SeatDTO>> carriages = new ArrayList<List<SeatDTO>>();
+        // Get reserved seats
+        List<Seat> listOfReservedSeats = ticketService.getBookingSeatsBySchedule(puzzledSchedulers.get(0));
 
         LOGGER.info(
                 "RESERVED SEATS IN CURRENT TRAIN # "
-                + selectedSchedule.getTrain().getName()
-                + " IS..."
+                        + train
+                        + " IS..."
         );
-
-
-        List<Seat> listOfReservedSeats = ticketService.getBookingSeatsBySchedule(selectedSchedule);
 
         for (Seat currentSeat: listOfReservedSeats) {
             LOGGER.info(
-                            "SEAT # "
+                    "SEAT # "
                             + currentSeat.getSeat()
                             + " IN CARRIAGE #"
                             + currentSeat.getCarriage()
@@ -157,25 +129,17 @@ public class TicketController {
             );
         }
 
+        // Move list to next controller via session
+        session.setAttribute("puzzledSchedulers", puzzledSchedulers);
 
-        // Create matrix of seats per carriage
-        // TODO: ADD THIS LEGACY CODE BESIDE CONTROLLER
-        int seatsCount = 1;
-        int row = selectedSchedule.getTrain().getCarriages();
-        int col = selectedSchedule.getTrain().getSeats().size() / selectedSchedule.getTrain().getCarriages();
-
-        for (int carriagesIterator = 1; carriagesIterator <= row; carriagesIterator++) {
-            ArrayList<SeatDTO> seats = new ArrayList<SeatDTO>();
-            seatsCount = 1;
-            for (int seatsIterator = 1; seatsIterator <= col; seatsIterator++) {
-                seats.add(new SeatDTO(carriagesIterator, seatsCount++));
-            }
-
-            carriages.add(seats);
-        }
-
-        model.addAttribute("selectedSchedule", selectedSchedule);
-        model.addAttribute("carriages", carriages);
+        // For rendering
+        model.addAttribute(
+                "carriages",
+                   ticketService.getSeatsMatrix(
+                           puzzledSchedulers.get(0).getTrain().getCarriages(),
+                           puzzledSchedulers.get(0).getTrain().getSeats().size() / puzzledSchedulers.get(0).getTrain().getCarriages()
+                   )
+        );
 
         return "train-seats-booking-info";
     }
@@ -183,133 +147,56 @@ public class TicketController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/confirmBooking", params = {"seat", "carriage", "scheduleId"})
-    public String confirmBooking(@RequestParam(value = "seat") Integer seat, @RequestParam(value = "carriage") Integer carriage,  @RequestParam(value = "scheduleId") Long scheduleId, Model model) {
+    @RequestMapping(value = "/confirmBooking", params = {"seat", "carriage"})
+    public String confirmBooking(HttpSession session, @RequestParam(value = "seat") Integer seat, @RequestParam(value = "carriage") Integer carriage, Model model) {
 
-        TicketDTO ticketDTO = new TicketDTO();
-        ticketDTO.setScheduleId(scheduleId);
+        // Status for booking result
+        String status = "";
 
-        SeatDTO seatDTO = new SeatDTO();
-        seatDTO.setSeat(seat);
-        seatDTO.setCarriage(carriage);
-        ticketDTO.setSeatDTO(seatDTO);
+        // Create puzzled tickets
+        List<TicketDTO> ticketDTOS = ticketService.getPuzzledTickets(
+                (List<Schedule>) session.getAttribute("puzzledSchedulers"),
+                seat,
+                carriage
+        );
 
+        // Authenticated user object
         UserDTO userDTO = userService.findAuthenticatedUserDTO();
 
-        LOGGER.info("LOADED USER TRY TI BUY " + userDTO.getLogin());
-        LOGGER.info(ticketDTO.getScheduleId() + " ; " + ticketDTO.getSeatDTO().getCarriage() + " " + ticketDTO.getSeatDTO().getSeat());
-
-        String result = "";
-
         try {
-            ticketService.add(
-                    ticketDTO,
-                    userService.findUserByEmail(userDTO.getLogin())
+            // Iterate each element of puzzled ticketDTO's and reserve it to user
+            for (int i = 0; i < ticketDTOS.size(); i++)
+                ticketService.add(
+                        ticketDTOS.get(i),
+                        userService.findUserByEmail(userDTO.getLogin())
+                );
+
+
+            model.addAttribute(
+                      "ticketInfo",
+                         ticketService.getBookingStatusInfo(ticketDTOS, userDTO)
             );
 
-            BookingTicketInfoDTO ticketInfoDTO = new BookingTicketInfoDTO();
-            ticketInfoDTO.setSeatNumber(seatDTO.getSeat());
-            ticketInfoDTO.setCarriageNumber(seatDTO.getCarriage());
+            status = "success";
 
-            ticketInfoDTO.setStationDepartureName(
-                    scheduleService.getById(ticketDTO.getScheduleId()).getStationDeparture().getName()
-            );
-
-            ticketInfoDTO.setStationArrivalName(
-                    scheduleService.getById(ticketDTO.getScheduleId()).getStationArrival().getName()
-            );
-
-            ticketInfoDTO.setDateDeparture(
-                    scheduleService.getById(ticketDTO.getScheduleId()).getDateDeparture()
-            );
-
-            ticketInfoDTO.setDateArrival(
-                    scheduleService.getById(ticketDTO.getScheduleId()).getDateArrival()
-            );
-
-            ticketInfoDTO.setUser(
-                            userDTO.getFirstName() + " "
-                            + userDTO.getLastName() + " ("
-                            + userDTO.getLogin() + ")"
-            );
-
-            model.addAttribute("ticketInfo", ticketInfoDTO);
-
-            result = "success";
         } catch (BookingTicketException e) {
-            e.printStackTrace();
 
-            BookingTicketInfoDTO ticketInfoDTO = new BookingTicketInfoDTO();
-            ticketInfoDTO.setSeatNumber(seatDTO.getSeat());
-            ticketInfoDTO.setCarriageNumber(seatDTO.getCarriage());
-
-            ticketInfoDTO.setUser(
-                    userDTO.getFirstName() + " "
-                            + userDTO.getLastName() + " ("
-                            + userDTO.getLogin() + ")"
+            // Create model attribute info status for booking problems
+            model.addAttribute(
+                    "ticketInfo",
+                       ticketService.getBookingStatusInfo(seat, carriage, userDTO)
             );
 
-
-            model.addAttribute("ticketInfo", ticketInfoDTO);
+            // Create model attribute - String (reason of booking problem)
             model.addAttribute("reason", e.getErrorMessage());
 
-            result = "problem";
+            // Change status
+            status = "problem";
         }
 
-        model.addAttribute("message", result);
-
+        model.addAttribute("message", status);
 
         return JspFormNames.BOOKING_TICKET_FORM_RESULT;
     }
-
-
-    /*
-
-
-    /*
-    @PostMapping("/buyTicket")
-    public String confirmTicketBooking(@ModelAttribute TicketInfoDTO ticketInfoDTO, HttpServletRequest request, Model model) {
-        Schedule schedule = new Schedule();
-
-        if (
-                ticketInfoDTO.getScheduleStationDepartureName() == null ||
-                        ticketInfoDTO.getScheduleStationArrivalName() == null ||
-                        ticketInfoDTO.getScheduleStationDepartureName() == null ||
-                        ticketInfoDTO.getUserLogin() == null ||
-                        ticketInfoDTO.getSeatCarriage() < 0 ||
-                        ticketInfoDTO.getSeatSeat() < 0
-                ) {
-            LOGGER.info("NOT VALID INPUT!");
-            return JspFormNames.BOOKING_TICKET_FORM;
-        }
-
-        schedule.setStationDeparture(
-                stationService.getStationByName(ticketInfoDTO.getScheduleStationDepartureName())
-        );
-
-        schedule.setStationArrival(
-                stationService.getStationByName(ticketInfoDTO.getScheduleStationArrivalName())
-        );
-
-        try {
-            schedule.setDateDeparture(
-                    UtilsManager.parseToDateTime(ticketInfoDTO.getScheduleDateDeparture())
-            );
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        TicketInfoDTO parsedTicketInfo = new TicketInfoDTO();
-        parsedTicketInfo.setUserLogin(ticketInfoDTO.getUserLogin());
-        parsedTicketInfo.setSeatSeat(ticketInfoDTO.getSeatSeat());
-        parsedTicketInfo.setSeatCarriage(ticketInfoDTO.getSeatCarriage());
-
-        request.getSession().setAttribute("ticketDTO", parsedTicketInfo);
-        model.addAttribute("schedules", scheduleService.getByStationsAndDate(schedule));
-
-        return JspFormNames.BOOKING_TICKET_LIST;
-    }
-
-   */
 
 }
