828b57999495ab9ceca7991805ae9ff4b9f3afa5
SlandShow <slandshow12345@mail.ru>
2018-10-22 14:15:35 +0300

    Add js files

---
+++ src/main/webapp/static/js/ajaxRequest.js
@@ -0,0 +1,157 @@
+var getRequest = function (urlSearching) {
+
+    var token = $("meta[name='_csrf']").attr("content");
+    var dataResponse = null;
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "GET",
+        url: urlSearching,
+        async: false
+    }).done(function (response) {
+        console.log(JSON.stringify(response));
+        dataResponse = response;
+    }).fail(function (qXHR, textStatus, errorThrown) {
+        var messageError = JSON.parse(qXHR.responseText)['message'].split('[MESSAGE]:')[1];
+        swal("Oops..", messageError, "error");
+        console.log('request: ', qXHR);
+        console.log('status text: ', textStatus);
+        console.log('thrown error: ', JSON.stringify(errorThrown));
+    });
+    return dataResponse;
+};
+
+var postRequest = function (data, urlSearching, successMessage) {
+
+    var token = $("meta[name='_csrf']").attr("content");
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "POST",
+        url: urlSearching,
+        data: JSON.stringify(data),
+    }).done(function (response) {
+        console.log(JSON.stringify(response));
+        swal("Good job!", successMessage, "success");
+    }).fail(function (qXHR, textStatus, errorThrown) {
+        var messageError = JSON.parse(qXHR.responseText)['message'].split('[MESSAGE]:')[1];
+        swal("Oops..", messageError, "error");
+        console.log('request: ', qXHR);
+        console.log('status text: ', textStatus);
+        console.log('thrown error: ', JSON.stringify(errorThrown));
+    });
+};
+
+var putRequest2 = function (data, urlSearching, successMessage, callback) {
+    var token = $("meta[name='_csrf']").attr("content");
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "PUT",
+        url: urlSearching,
+        data: JSON.stringify(data),
+    }).done(function (response) {
+        console.log(JSON.stringify(response));
+        swal("Good job!", successMessage, "success");
+        callback();
+    }).fail(function (qXHR, textStatus, errorThrown) {
+        var messageError = JSON.parse(qXHR.responseText)['message'].split('[MESSAGE]:')[1];
+        swal("Oops..", messageError, "error");
+        console.log('request: ', qXHR);
+        console.log('status text: ', textStatus);
+        console.log('thrown error: ', JSON.stringify(errorThrown));
+    });
+};
+
+var deleteRequest2 = function (urlSearching, successMessage, callback) {
+    var token = $("meta[name='_csrf']").attr("content");
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "DELETE",
+        url: urlSearching,
+        async: false
+    }).done(function (response) {
+        console.log(JSON.stringify(response));
+        swal("Good job!", successMessage, "success");
+        callback();
+    }).fail(function (qXHR, textStatus, errorThrown) {
+        var messageError = JSON.parse(qXHR.responseText)['message'].split('[MESSAGE]:')[1];
+        swal("Oops..", messageError, "error");
+        console.log('request: ', qXHR);
+        console.log('status text: ', textStatus);
+        console.log('thrown error: ', JSON.stringify(errorThrown));
+    });
+};
+
+var putRequest = function (data, urlSearching, successMessage) {
+    var token = $("meta[name='_csrf']").attr("content");
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "PUT",
+        url: urlSearching,
+        data: JSON.stringify(data),
+    }).done(function (response) {
+        console.log(JSON.stringify(response));
+        swal("Good job!", successMessage, "success");
+        callback();
+    }).fail(function (qXHR, textStatus, errorThrown) {
+        var messageError = JSON.parse(qXHR.responseText)['message'].split('[MESSAGE]:')[1];
+        swal("Oops..", messageError, "error");
+        console.log('request: ', qXHR);
+        console.log('status text: ', textStatus);
+        console.log('thrown error: ', JSON.stringify(errorThrown));
+    });
+};
+
+var deleteRequest = function (urlSearching, successMessage) {
+    var token = $("meta[name='_csrf']").attr("content");
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "DELETE",
+        url: urlSearching,
+        async: false
+    }).done(function (response) {
+        console.log(JSON.stringify(response));
+        swal("Good job!", successMessage, "success");
+        callback();
+    }).fail(function (qXHR, textStatus, errorThrown) {
+        var messageError = JSON.parse(qXHR.responseText)['message'].split('[MESSAGE]:')[1];
+        swal("Oops..", messageError, "error");
+        console.log('request: ', qXHR);
+        console.log('status text: ', textStatus);
+        console.log('thrown error: ', JSON.stringify(errorThrown));
+    });
+};
