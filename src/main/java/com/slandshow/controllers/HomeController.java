c3b775b3b15b777ce4e5d9ce9ea55cde9c6c8eca
SlandShow <slandshow12345@mail.ru>
2018-10-24 00:20:56 +0300

    Clear code in home controller

--- src/main/java/com/slandshow/controllers/HomeController.java
+++ src/main/java/com/slandshow/controllers/HomeController.java
@@ -1,8 +1,13 @@
 package com.slandshow.controllers;
 
+import com.slandshow.service.MessageQueueService;
 import com.slandshow.utils.JspFormNames;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @Controller
 public class HomeController {
